e2ba40d0b739d84b8d0163280f92f7ea064ccec3
elf-G <46258939+elf-G@users.noreply.github.com>
2018-12-31 12:07:00 +0800

    Update fer_config.py

--- fer_config.py
+++ fer_config.py
@@ -2,25 +2,25 @@
 
 #相关配置
 
-image_train_path='/home/user/gjj/data/fer2013/train/'
-label_train_path='/home/user/gjj/data/fer2013/labels_train.txt'
-tfRecord_train='/home/user/gjj/data/fer2013/fer2013_train.tfrecords'
-image_valid_path='/home/user/gjj/data/fer2013/valid/'
-label_valid_path='/home/user/gjj/data/fer2013/labels_valid.txt'
-tfRecord_valid='/home/user/gjj/data/fer2013/fer2013_valid.tfrecords'
-image_test_path='/home/user/gjj/data/fer2013/test/'
-label_test_path='/home/user/gjj/data/fer2013/labels_test.txt'
-tfRecord_test='/home/user/gjj/data/fer2013/fer2013_test.tfrecords'
+image_train_path='/home/user/data/fer2013/train/'
+label_train_path='/home/user/data/fer2013/labels_train.txt'
+tfRecord_train='/home/user/data/fer2013/fer2013_train.tfrecords'
+image_valid_path='/home/user/data/fer2013/valid/'
+label_valid_path='/home/user/data/fer2013/labels_valid.txt'
+tfRecord_valid='/home/user/data/fer2013/fer2013_valid.tfrecords'
+image_test_path='/home/user/data/fer2013/test/'
+label_test_path='/home/user/data/fer2013/labels_test.txt'
+tfRecord_test='/home/user/data/fer2013/fer2013_test.tfrecords'
 
-data_path='/home/user/gjj/data/fer2013/fer2013.csv'
-train_data_path='/home/user/gjj/data/fer2013/fer2013_train.csv'
-valid_data_path='/home/user/gjj/data/fer2013/fer2013_valid.csv'
-test_data_path='/home/user/gjj/data/fer2013/fer2013_test.csv'
+data_path='/home/user/data/fer2013/fer2013.csv'
+train_data_path='/home/user/data/fer2013/fer2013_train.csv'
+valid_data_path='/home/user/data/fer2013/fer2013_valid.csv'
+test_data_path='/home/user/data/fer2013/fer2013_test.csv'
 
 
-data_file='/home/user/gjj/data/fer2013'
+data_file='/home/user/data/fer2013'
 #模型存储路径
-MODEL_SAVE_PATH="/home/user/gjj/model/fer"
+MODEL_SAVE_PATH="/home/user/model/fer"
 #模型名称
 MODEL_NAME="fer_model"
 #图片高
